4c24ac677bdbb0b9747b061a22a21342017a6040
Takuro Murata <include.takuro@gmail.com>
2020-04-30 22:48:53 +0900

    gormでCountとったりCreateするテスト

--- main.go
+++ main.go
@@ -22,11 +22,20 @@ func main() {
     }
     size := c.Query("size")
     i, _ := strconv.Atoi(size)//文字列→Int
-    c.String(200, "Park Pref = %s Size = %d", pref, i)
+    db := model.GetDB()
+    var count int = 0
+    db.Model(&model.Park{}).Count(&count)
+    c.String(200, "Park Pref = %s Size = %d Count = %d", pref, i, count)
   })
 
   router.GET("/parks/:id", func(c *gin.Context) {
     c.String(200, "Park ID = " + c.Param("id"))
+  })
+
+  router.POST("/parks", func(c *gin.Context) {
+    db := model.GetDB()
+    db.Create(&model.Park{Name: "Test Park", Lat: 35.734621, Lon: 139.7098774})
+    db.Close()
   })
 
   router.Run()
