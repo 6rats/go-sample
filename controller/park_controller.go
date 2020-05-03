ccca23c7d6cb65bfdcaf7656971c748ee3f318e4
Takuro Murata <include.takuro@gmail.com>
2020-05-04 07:02:00 +0900

    validatorによるバリデーションの実験、http status codeをnet/httpを使って実装

--- controller/park_controller.go
+++ controller/park_controller.go
@@ -3,13 +3,29 @@ package controller
 import (
   "github.com/gin-gonic/gin"
   "local.packages/model"
+  "net/http"
+  "log"
 )
 
 type UserController struct{}
 
+type IndexParams struct {
+  Pref int `form:"pref" binding:"required,min=1,max=47"`
+}
+
 func (uc UserController) Index(c *gin.Context) {
-  db := model.GetDB()
-  var parks []model.Park
-  db.Find(&parks)
-  c.JSON(200, parks)
+
+  var params IndexParams
+
+  if err := c.ShouldBind(&params); err != nil {
+    c.JSON(http.StatusBadRequest, err.Error())
+  } else {
+
+    log.Println(params.Pref)
+
+    var parks []model.Park
+    db := model.GetDB()
+    db.Find(&parks)
+    c.JSON(http.StatusOK, parks)
+  }
 }
